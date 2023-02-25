4c252034f17d31016e8c4fa02083333581f53079
Nils Diekmann <nils@diekmann-germany.de>
2023-02-25 20:04:26 +0100

    - fixed path; - generate files

--- golang/generate-go.sh
+++ golang/generate-go.sh
@@ -7,11 +7,12 @@ export PATH="$PATH:$(go env GOPATH)/bin"
 protobase=protos
 protopath=${protobase}/kinnekode
 projects=( $(ls ${protopath}) )
+outputpath=golang
 
 for i in "${projects[@]}"
 do
     find ${protopath} -name *.proto \
-	-exec tools/protoc/win64/bin/protoc.exe --proto_path=${protobase} --go_out=go --go_opt=paths=source_relative --go-grpc_out=go --go-grpc_opt=paths=source_relative {} +
+	-exec tools/protoc/win64/bin/protoc.exe --proto_path=${protobase} --go_out=${outputpath} --go_opt=paths=source_relative --go-grpc_out=${outputpath} --go-grpc_opt=paths=source_relative {} +
 	
 	echo "Generated protos for: $i"
 done
